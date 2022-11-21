93cfda686599eb7914a388a70e64f4ae3cb6039e
kingPasha <nextunesorg@gmail.com>
2022-11-21 19:58:29 +0330

    fixed max width of header

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -5,7 +5,7 @@ import { FiSearch } from "react-icons/fi";
 
 const Search = () => {
   return (
-    <div className="max-w-[1024px] mx-auto  px-2 py-5 flex flex-col">
+    <div className="max-w-[1024px] mx-auto  px-2 py-5 flex flex-col bg-red-400">
       <div className="bg-slate-800 p-3 flex justify-center items-center rounded">
         <FiSearch color="white" size={30} className='cursor-pointer' />
         <input
