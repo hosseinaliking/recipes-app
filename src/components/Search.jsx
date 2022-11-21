c63f13debbcb7953f9abaccab386d3ab188d3e99
kingPasha <nextunesorg@gmail.com>
2022-11-21 19:52:11 +0330

    completed all search input

---
+++ src/components/Search.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+//* Icons *//
+import { FiSearch } from "react-icons/fi";
+
+const Search = () => {
+  return (
+    <div className="max-w-[1024px] mx-auto  px-2 py-5 flex flex-col">
+      <div className="bg-slate-800 p-3 flex justify-center items-center rounded">
+        <FiSearch color="white" size={30} className='cursor-pointer' />
+        <input
+          type="text"
+          placeholder="Search here ..."
+          className="block w-[100%] md:w-[50%] p-3 outline-none bg-transparent caret-white text-white"
+        />
+      </div>
+
+      <ul></ul>
+    </div>
+  );
+};
+
+export default Search;
