cdcf9bb91ecf7ab2ec7284c802b3af8586ae601c
kingPasha <nextunesorg@gmail.com>
2022-11-21 19:52:55 +0330

    fixed max width in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ import Logo from '../asset/logo.png'
 
 const Header = () => {
   return (
-    <header className='w-[1024px] mx-auto px-2 py-5 flex items-center gap-x-3 font-bold'>
+    <header className='max-w-[1024px] mx-auto px-2 py-5 flex items-center gap-x-3 font-bold'>
       <div className="">
         <img src={Logo} alt="logo of website" className='h-12 w-12' /> 
       </div>
